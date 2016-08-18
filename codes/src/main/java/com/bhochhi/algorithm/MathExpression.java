812c01247364fb624792b7056b4eeb71af0e41b2
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2016-08-18 06:36:07 -0500

    in progress

--- codes/src/main/java/com/bhochhi/algorithm/MathExpression.java
+++ codes/src/main/java/com/bhochhi/algorithm/MathExpression.java
@@ -2,25 +2,25 @@ package com.bhochhi.algorithm;
 
 public class MathExpression {
 
-	
 	public static void main(String[] args) {
 		String exprn = "2+3";
 		System.out.println(compute(exprn));
 	}
 
-	private static char[] compute(String exprn) {
-		char[] expChars = exprn.toCharArray(); 
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	class Node{
-		int key;
-		String name;
-		
-		Node parentNode;
-		Node leftNode;
-		Node rightNode;
-		
+	private static int compute(String exprn) {
+		char[] expChars = exprn.toCharArray();
+		NodeT tree = new NodeT();
+		NodeT currentNode = tree;
+		for (char c : expChars) {
+			if (c == '(') {
+				currentNode.leftNode = new NodeT();
+				currentNode = currentNode.leftNode;
+			} else if (c == ')') {
+				currentNode = currentNode.parentNode;
+			} else if (c == '+' || c == '-' || c == '*' || c == '/') {
+				
+			}
+			return 0;
+		}
 	}
 }
