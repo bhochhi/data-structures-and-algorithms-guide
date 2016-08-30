c19403e8521a5ac25ad7baa0393ea52f1e046249
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2016-08-30 06:56:16 -0500

    wip

--- codes/src/main/java/com/bhochhi/algorithm/MathExpression.java
+++ codes/src/main/java/com/bhochhi/algorithm/MathExpression.java
@@ -1,27 +1,37 @@
 package com.bhochhi.algorithm;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
-import org.junit.runner.Computer;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MathExpression {
 
 	public static void main(String[] args) {
-		String exprn = "2+3";
+		String exprn = "2+ 3";
 		NodeT tree = parse(exprn);
 		printTree(tree);
-	}
+	} 
 	
 	
 	private static void printTree(NodeT tree) {
-		// TODO Auto-generated method stub
+		System.out.println(tree.value);
+		while(tree.hasChild()){
+			List<NodeT> nodes = tree.getChildren();
+			for(NodeT n: nodes){
+				printTree(n);
+			}
+			
+		}
 		
 	}
 
 	private static NodeT parse(String exprn) {
-		char[] expChars = exprn.toCharArray();
+		System.out.println((exprn.toCharArray().length));
+		Collections.reverse(expChars);
 		NodeT root = new NodeT();
+//		for(String c : expChars){
+//			System.out.println(c);
+//		}
 		
 		return root;
 	}
--- codes/src/main/java/com/bhochhi/algorithm/NodeT.java
+++ codes/src/main/java/com/bhochhi/algorithm/NodeT.java
@@ -1,10 +1,29 @@
 package com.bhochhi.algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NodeT {
 	String value;
 	String name;
-	
+
 	NodeT parentNode;
 	NodeT leftNode;
 	NodeT rightNode;
+
+	public boolean hasChild() {
+		return leftNode != null || rightNode != null;
+	}
+
+	public List<NodeT> getChildren() {
+		List<NodeT> nodes = new ArrayList<NodeT>();
+		if (leftNode != null) {
+			nodes.add(leftNode);
+		}
+		if (rightNode != null) {
+			nodes.add(rightNode);
+		}
+		return nodes;
+	}
+
 }
